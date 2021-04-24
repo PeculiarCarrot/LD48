4db9dd53edbc1a0636abd6d5af66d0903d69189a
Austin Harman <peculiarcarrot@gmail.com>
2021-04-24 00:04:15 -0400

    root moves without a million points wooo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
     public LineRenderer root;
     public float timeBetweenNodes = .2f;
     float timeSinceLastRootNode;
+    public int maxNodes = 10;
 
     void Start()
     {
@@ -19,7 +20,22 @@ public class GameController : MonoBehaviour
         {
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
-            root.positionCount++; root.SetPosition(root.positionCount - 1, mousePos);
+            if(root.positionCount < maxNodes)
+            {
+                root.positionCount++;
+            }
+            else
+			{
+                var arr = new Vector3[maxNodes];
+                root.GetPositions(arr);
+				for (int i = 0; i < maxNodes - 1; i++)
+				{
+                    arr[i] = arr[i + 1];
+				}
+                root.SetPositions(arr);
+			}
+
+            root.SetPosition(root.positionCount - 1, mousePos);
             timeSinceLastRootNode = 0;
         }
         else
