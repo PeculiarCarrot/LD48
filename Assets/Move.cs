3cd8b6462b7bbda9a22789480775087f59ed87d7
Austin Harman <peculiarcarrot@gmail.com>
2021-04-23 22:06:50 -0400

    root

---
+++ Assets/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 dir;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = transform.position + dir * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public LineRenderer root;
+    public float timeBetweenNodes = .2f;
+    float timeSinceLastRootNode;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timeSinceLastRootNode > timeBetweenNodes)
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            root.positionCount++; root.SetPosition(root.positionCount - 1, mousePos);
+            timeSinceLastRootNode = 0;
+        }
+        else
+            timeSinceLastRootNode += Time.deltaTime;
+    }
+}
